1d39331c72a0261f48cafbba2d7936eae9c46482
Ravi30T <gatlaravi85@gmail.com>
2024-11-29 08:29:21 +0530

    Frontend Development Done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import {Switch, Route} from 'react-router-dom'
+import Home from './components/Home';
+import ApplicationForm from './components/ApplicationForm';
+import ApplicationDetails from './components/ApplicationDetails';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/new-application" component={ApplicationForm} />
+      <Route exact path={`/application-details/:id`} component={ApplicationDetails} />
+    </Switch>
+  )
 }
 
 export default App;
